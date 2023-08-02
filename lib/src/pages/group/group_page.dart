d5ef804783383bf99ae9a17dadf4133791d4cfb0
med363 <blibechmedamine@gmail.com>
2023-08-02 14:00:31 +0100

    connet with db

--- lib/src/pages/group/group_page.dart
+++ lib/src/pages/group/group_page.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
+import 'package:chat/src/pages/group/msg_model.dart';
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
-
 
 class GroupPage extends StatefulWidget {
   final String name;
@@ -11,6 +13,8 @@ class GroupPage extends StatefulWidget {
 }
 
 class _GroupPageState extends State<GroupPage> {
+  IO.Socket? socket;
+  List<MsgModel> listMsg = [];
   @override
   void initState() {
     // TODO: implement initState
@@ -18,49 +22,55 @@ class _GroupPageState extends State<GroupPage> {
     connect();
   }
 
-  void connect(){
+  void connect() {
     // Dart client
-  IO.Socket socket = IO.io('http://localhost:3000');
-  socket.onConnect((_) {
-    print('connect into frontend');
-    socket.emit('sendMsg', 'test emit event');
-  });
-  socket.on('event', (data) => print(data));
-  socket.onDisconnect((_) => print('disconnect'));
-  socket.on('fromServer', (_) => print(_));
+    socket = IO.io('http://localhost:3000', <String, dynamic>{
+      "transports": ["websocket"],
+      "autoConnect": false,
+    });
+    socket!.connect();
+    socket!.onConnect((_) => print('connect into frontend'));
+  }
+
+  void sendMsg(String msg, String SenderName) {
+   socket!.emit('sendMsg', {
+   "type": "OwnMsg",
+   "msg": msg,
+   "SenderName": SenderName});
+
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-   appBar: AppBar(
-title: const Text("Anonymous grp"),
-   ) ,
-   body: Column(
-    children: [
-      Expanded(child: Container(),
+      appBar: AppBar(
+        title: const Text("Anonymous grp"),
       ),
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Type here ...",
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide()
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+            child: Row(children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Type here ...",
+                    border: OutlineInputBorder(borderSide: BorderSide()),
                   ),
                 ),
-                ),
-                ),
-                IconButton(onPressed: (){},
-                 icon: const Icon(Icons.send,
-                 color: Colors.teal,
-                 ))
-          ]),)
-    ],
-    
-   ),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.send,
+                    color: Colors.teal,
+                  ))
+            ]),
+          )
+        ],
+      ),
     );
   }
 }
