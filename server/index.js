611422865704db2e420e0f50bec5ddb55a7bff03
med amine blibech <56481634+med363@users.noreply.github.com>
2023-08-08 03:12:48 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
+// const express = require('express');
+// const http = require('http');
+// const app = express();
+// const Server = http.createServer(app);
+// const io = require('socket.io')(Server);
+// app.get('/',(req,res)=>{
+//     res.json({msg:"API working successfully"})
+// })
+
+// //channel
+// io.on("connection",(client)=>{
+//     console.log("new client connected");
+//     client.on('msg',(data)=>{
+//         console.log(data);
+//         // client.broadcast.emit('res',data)
+//         client.to(client.id).emit('res',data)
+
+//     })
+// })
+
+//      Server.listen(3000)
 const express = require('express');
 const {createServer} = require("http");
 const { Server } = require("socket.io");
@@ -15,7 +36,8 @@ app.route("/").get((req,res)=>{
         socket.on("sendMsg",(msg)=>{
             console.log("msg", msg);
             // socket.emit("sendMsgServer",{...msg, type:"otherMsg"})
-            io.to("anomynous_group").emit("sendMsgServer",{...msg, type:"otherMsg"})
+            // socket.broadcast.emit("sendMsgServer",{...msg, type:"otherMsg"})
+            io.to("anomynous_group").emit("sendMsgServer",{...msg,type:"otherMsg"});
         })
         //....
     });
